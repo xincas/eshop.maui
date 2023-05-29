dd5884c9c5d1aa9dce7d3af07f108c89e0410023
xincas <ismrah.25@gmail.com>
2023-05-29 08:11:15 +0300

    Revert "mono settings, entry underline off"
    This reverts commit f83b489c0d3be32404d4ea4fa58f2e2873910670.

--- eshop.mobile/Platforms/Android/MainActivity.cs
+++ eshop.mobile/Platforms/Android/MainActivity.cs
@@ -1,11 +1,10 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 
 namespace Eshop.Mobile.Platforms.Android;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true,
-    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
