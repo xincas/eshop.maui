f83b489c0d3be32404d4ea4fa58f2e2873910670
xincas <ismrah.25@gmail.com>
2023-05-29 08:10:51 +0300

    mono settings, entry underline off

---
+++ eshop.mobile/Platforms/Android/CustomHandlers/CustomEntryHandler.cs
@@ -0,0 +1,12 @@
+﻿using AndroidX.AppCompat.Widget;
+
+namespace Eshop.Mobile.Platforms.Android.CustomHandlers;
+
+public class CustomEntryHandler : Microsoft.Maui.Handlers.EntryHandler
+{
+    protected override void ConnectHandler(AppCompatEditText platformView)
+    {
+        platformView.Background = null;
+        base.ConnectHandler(platformView);
+    }
+}
