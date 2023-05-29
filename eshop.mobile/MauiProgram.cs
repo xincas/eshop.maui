2ad6824b3678d9bf090c3befd2e569e397874a38
xincas <ismrah.25@gmail.com>
2023-05-29 08:17:31 +0300

    custom entry for android and adding sentry

--- eshop.mobile/MauiProgram.cs
+++ eshop.mobile/MauiProgram.cs
@@ -10,18 +10,25 @@ namespace Eshop.Mobile;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseSentry(options =>
+            {
+                options.Dsn =
+                    "https://9fb4e74cdb944ce48c03d63ace8c9160@o4505240041947136.ingest.sentry.io/4505240042930176";
+                //options.Debug = true;
+                options.TracesSampleRate = 1.0;
+            })
             .UseMauiCommunityToolkit()
             .UseUraniumUI()
             .UseUraniumUIMaterial()
             .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("Font-Awesome-6-Brands-Regular.otf", "FABrands");
                 //fonts.AddFont("Font-Awesome-6-Regular.otf", "FARegular");
                 //fonts.AddFont("Font-Awesome-6-Solid.otf", "FASolid");
@@ -32,6 +39,9 @@ public static class MauiProgram
             .ConfigureMauiHandlers(handlers =>
             {
                 handlers.AddInputKitHandlers();
+#if ANDROID
+                handlers.AddHandler(typeof(Entry), typeof(Eshop.Mobile.Platforms.Android.CustomHandlers.CustomEntryHandler));
+#endif
             })
             .RegisterPages()
             .RegisterViewModels()
@@ -41,11 +51,11 @@ public static class MauiProgram
         builder.Services.AddLogging(configure =>
         {
             configure.AddDebug();
-                //.AddFilter("com.companyname.eshop.mobile", LogLevel.Trace)
-                //.AddFilter("Microsoft", LogLevel.Warning);
+            //.AddFilter("com.companyname.eshop.mobile", LogLevel.Trace)
+            //.AddFilter("Microsoft", LogLevel.Warning);
         });
 #endif
 
         return builder.Build();
-	}
+    }
 }
---
+++ eshop.mobile/Platforms/Android/CustomHandlers/CustomEntryHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Eshop.Mobile.Platforms.Android.CustomHandlers;
+
+public class CustomEntryHandler : Microsoft.Maui.Handlers.EntryHandler
+{
+    protected override void ConnectHandler(AppCompatEditText platformView)
+    {
+        platformView.Background = null;
+        base.ConnectHandler(platformView);
+    }
+}
