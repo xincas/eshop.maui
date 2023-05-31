5bed5a016c1e3170f364656e4ac7436605932f23
xincas <ismrah.25@gmail.com>
2023-05-31 03:50:57 +0300

    fix null pointer exception on android

--- eshop.mobile/Platforms/Android/CustomHandlers/CustomEntryHandler.cs
+++ eshop.mobile/Platforms/Android/CustomHandlers/CustomEntryHandler.cs
@@ -1,10 +1,15 @@
-﻿namespace Eshop.Mobile.Platforms.Android.CustomHandlers;
+﻿using Android.Graphics;
+using Android.Graphics.Drawables;
+using AndroidX.AppCompat.Widget;
+using Color = Android.Graphics.Color;
+
+namespace Eshop.Mobile.Platforms.Android.CustomHandlers;
 
 public class CustomEntryHandler : Microsoft.Maui.Handlers.EntryHandler
 {
     protected override void ConnectHandler(AppCompatEditText platformView)
     {
-        platformView.Background = null;
+        platformView.Background = new ColorDrawable(new Color(255, 255, 255, 0));
         base.ConnectHandler(platformView);
     }
 }
