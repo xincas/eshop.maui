463099257f8fe69c8666e124238d46850347aa8d
xincas <ismrah.25@gmail.com>
2023-05-31 03:54:00 +0300

    add popup with accept cancel buttons (popup with 2 choice)

--- eshop.mobile/Services/Dialog/DialogService.cs
+++ eshop.mobile/Services/Dialog/DialogService.cs
@@ -10,4 +10,11 @@ public class DialogService : IDialogService
 
         return Current.MainPage.DisplayAlert(title, message, buttonLabel);
     }
+
+    public Task<bool> ShowAlertAsync(string message, string title, string acceptButton, string cancelButton)
+    {
+        if (Current is null || Current.MainPage is null) return Task.FromResult(false);
+
+        return Current.MainPage.DisplayAlert(title, message, acceptButton, cancelButton);
+    }
 }
--- eshop.mobile/Services/Dialog/IDialogService.cs
+++ eshop.mobile/Services/Dialog/IDialogService.cs
@@ -3,4 +3,5 @@
 public interface IDialogService
 {
     Task ShowAlertAsync(string message, string title, string buttonLabel);
+    Task<bool> ShowAlertAsync(string message, string title, string acceptButton, string cancelButton);
 }
