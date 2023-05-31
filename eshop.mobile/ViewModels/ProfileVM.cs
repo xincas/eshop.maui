14cb6c2a368cef75276f9bef2c7efe52224a52da
xincas <ismrah.25@gmail.com>
2023-05-31 03:56:02 +0300

    accept before logout

--- eshop.mobile/ViewModels/ProfileVM.cs
+++ eshop.mobile/ViewModels/ProfileVM.cs
@@ -4,6 +4,7 @@ using CommunityToolkit.Mvvm.Input;
 using Eshop.Mobile.Models;
 using Eshop.Mobile.Models.ApiResponse;
 using Eshop.Mobile.Services.Auth;
+using Eshop.Mobile.Services.Dialog;
 using Eshop.Mobile.Services.Navigation;
 using Eshop.Mobile.Services.Order;
 using Eshop.Mobile.ViewModels.Base;
@@ -14,23 +15,33 @@ public partial class ProfileVM : ViewModelBase
 {
     private readonly IAuthService _authService;
     private readonly IOrderService _orderService;
+    private readonly IDialogService _dialogService;
 
     ObservableCollection<Order> _orders;
 
     public IReadOnlyList<Order> Orders => _orders;
     [ObservableProperty] private User _user;
 
-    public ProfileVM(INavigationService navigationService, IAuthService authService, IOrderService order) : base(
+    public ProfileVM(INavigationService navigationService, IAuthService authService, IOrderService order,
+        IDialogService dialog) : base(
         navigationService)
     {
         _authService = authService;
         _orderService = order;
+        _dialogService = dialog;
 
         _orders = new ObservableCollection<Order>();
     }
 
     [RelayCommand]
-    async void LogOut() => await _authService.NavigateToLoginPageAsync();
+    async void LogOut()
+    {
+        var result =
+            await _dialogService.ShowAlertAsync("Вы действительно хотите выйти?", "Подтверждение", "Да", "Нет");
+
+        if (result)
+            await _authService.NavigateToLoginPageAsync();
+    }
 
     public async override Task InitializeAsync()
     {
